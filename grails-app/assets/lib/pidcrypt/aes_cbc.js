bfddc659559569e13380437840c07d2bcb9ddc20
omid <omid.mt@gmail.com>
2015-10-11 09:24:44 +0330

    Remove extra char

--- grails-app/assets/lib/pidcrypt/aes_cbc.js
+++ grails-app/assets/lib/pidcrypt/aes_cbc.js
@@ -1,4 +1,4 @@
-/*----------------------------------------------------------------------------*/ s
+/*----------------------------------------------------------------------------*/
 // Copyright (c) 2009 pidder <www.pidder.com>
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
